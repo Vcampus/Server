77519d8e46a4aad8b2903865243b62a79f8ebb29
Heyongdong <heyongdonghe@qq.com>
2015-08-29 17:28:32 +0800

    用来试试连朴哥的

--- src/com/seu/server/ServerMain.java
+++ src/com/seu/server/ServerMain.java
@@ -3,9 +3,9 @@ package com.seu.server;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import javax.security.auth.login.LoginException;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,14 +20,41 @@ public class ServerMain {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+
 		System.out.println("Server: Server running..");
-		ServerListener serverListener=new ServerListener();
-		serverListener.start();
+		//ServerListener serverListener=new ServerListener();
+		//serverListener.start();
+
+
 		//TestClient testClient = new TestClient();
 		//DbHelper db=new DbHelper();
 
 		//String s=db.execute("select * from grade",1).toString();
-		ServerClient s=new ServerClient();
+
+
+		//ServerClient s=new ServerClient();
+		try {
+			Socket s =new Socket("192.168.1.101",12345);
+			DataOutputStream out =new DataOutputStream(s.getOutputStream());
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			BufferedReader bf = new BufferedReader(
+					new InputStreamReader(System.in));
+			//注意不能直接用DataInputStream来封装标准输入，原因前文已提到
+
+
+			out.writeUTF("asdasfdasdf");
+			while(true){
+				String str =  bf.readLine();
+				out.writeChar('s');
+				out.flush();
+			}
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 		//Message msg = MessageFactory.getDefaultAskMessage("11111111",
 		//		"INSERT INTO `course`.`user` (`username`, `digested_password`, `uuid`) VALUES ('Test2', '1232442', '2123312')", Message.TYPE.UPDATE);
 //		String username = "Me";
